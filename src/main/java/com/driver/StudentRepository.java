303a3f4cd4ff27ee45eb3315e41d96322c845657
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-03-19 20:45:05 +0530

    check

--- src/main/java/com/driver/StudentRepository.java
+++ src/main/java/com/driver/StudentRepository.java
@@ -19,40 +19,53 @@ public class StudentRepository {
 
     public void saveStudent(Student student){
         // your code goes here
+        studentMap.put(student.getName().trim(), student);
     }
 
     public void saveTeacher(Teacher teacher){
         // your code goes here
+        teacherMap.put(teacher.getName().trim(), teacher);
     }
 
     public void saveStudentTeacherPair(String student, String teacher){
-        if(studentMap.containsKey(student) && teacherMap.containsKey(teacher)){
+        if(studentMap.containsKey(student.trim()) && teacherMap.containsKey(teacher.trim())){
             // your code goes here
+            List<String> students = teacherStudentMapping.getOrDefault(teacher, new ArrayList<>());
+            students.add(student.trim());
+            teacherStudentMapping.put(teacher.trim(), students);
         }
     }
 
     public Student findStudent(String student){
         // your code goes here
+        return studentMap.get(student.trim());
     }
 
     public Teacher findTeacher(String teacher){
         // your code goes here
+        return teacherMap.get(teacher.trim());
     }
 
     public List<String> findStudentsFromTeacher(String teacher){
         // your code goes here
         // find student list corresponding to a teacher
+        return teacherStudentMapping.getOrDefault(teacher.trim(), new ArrayList<>());
     }
 
     public List<String> findAllStudents(){
         // your code goes here
+        return new ArrayList<>(studentMap.keySet());
     }
 
     public void deleteTeacher(String teacher){
         // your code goes here
+        teacherMap.remove(teacher.trim());
+        teacherStudentMapping.remove(teacher.trim());
     }
 
     public void deleteAllTeachers(){
         // your code goes here
+        teacherMap.clear();
+        teacherStudentMapping.clear();
     }
 }
